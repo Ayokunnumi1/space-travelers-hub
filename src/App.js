308f4c61db7f01e14870984249b35054b4580670
MohammadYaser <yasersafi19@outlook.com>
2023-10-02 21:31:02 +0430

    Create header for project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,47 @@
 import './App.css';
-// import { Route, Routes } from 'react-router-dom';
+import {
+  BrowserRouter as Router, Route, Routes, Link,
+} from 'react-router-dom';
+
+import Rockets from './components/Rockets';
+import Missions from './components/Missions';
+import MyProfile from './components/MyProfile';
+import logo from './assets/logo.png';
 
 function App() {
   return (
     <div className="App">
-      <h1>Space travelers</h1>
+      <Router>
+        <header>
+          <div className="header-container">
+            <div className="header-left">
+              <div className="logo-container">
+                <img src={logo} className="logo" alt="header logo" />
+                <h1 className="header-title">Space Travelars Hub</h1>
+              </div>
+              <nav>
+                <ul className="links">
+                  <li>
+                    <Link to="/">Rockets</Link>
+                  </li>
+                  <li>
+                    <Link to="/missions">Missions</Link>
+                  </li>
+                  <li>
+                    <Link to="/myprofile">My Profile</Link>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+          </div>
+        </header>
+
+        <Routes>
+          <Route path="/" element={<Rockets />} />
+          <Route path="/missions" element={<Missions />} />
+          <Route path="/myprofile" element={<MyProfile />} />
+        </Routes>
+      </Router>
     </div>
   );
 }

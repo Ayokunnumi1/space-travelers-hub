a85a052392964e7157f16d68c5a1f6143fd38dfe
Ayokunnumi1 <omololuayk@gmail.com>
2023-10-02 15:26:31 +0100

    Install React Router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <h1>Space Traveler</h1>
+      <Routes>
+        <Route>
+          <h1>Space Traveler</h1>
+        </Route>
+      </Routes>
     </div>
   );
 }

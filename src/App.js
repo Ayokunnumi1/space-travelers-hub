6ff2f9e9bf3de1ba93225513e26d5dfc46e7d129
Ayokunnumi1 <omololuayk@gmail.com>
2023-10-02 15:52:30 +0100

    Install linters

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Space traveler's</h1>
+      <h1>Space travelers</h1>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import App from './App';
 import store from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -10,8 +10,8 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
-       <App />    
+        <App />
       </Provider>
     </BrowserRouter>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
